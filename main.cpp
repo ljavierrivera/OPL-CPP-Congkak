f6cc98e431a844d5988a67b615c825749f258626
Javier Rivera <ljavierrivera@gmail.com>
2011-10-13 01:07:43 -0400

    adding main.cpp (no code written yet)

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: javier.rivera
+ *
+ * Created on October 13, 2011, 12:36 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
