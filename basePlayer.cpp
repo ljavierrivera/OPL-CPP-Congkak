e163bba044f328bca8d28e864beddb13864f7f8b
Javier Rivera <ljavierrivera@gmail.com>
2011-10-13 19:55:40 -0400

    adding basePlayer.cpp file with very basic functionality

---
+++ basePlayer.cpp
@@ -0,0 +1,39 @@
+#include "basePlayer.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+basePlayer::basePlayer()
+{
+    basePlayer::setName("");
+    basePlayer::setNumWins(0);
+}
+
+void basePlayer::setName(string a_name)
+{
+    basePlayer::m_player_name = a_name;
+}
+
+string basePlayer::getName()
+{
+    return basePlayer::m_player_name;
+}
+
+void basePlayer::setNumWins(int a_num_wins)
+{
+    basePlayer::m_num_wins = a_num_wins;
+}
+
+int basePlayer::getNumWins()
+{
+    return basePlayer::m_num_wins;
+}
+
+basePlayer::~basePlayer()
+{
+    basePlayer::m_player_name.clear();
+    basePlayer::m_num_wins = 0;
+}
