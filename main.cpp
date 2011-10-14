2834040b8dea133779bc5c3ba6b4140cc359c62d
Javier Rivera <ljavierrivera@gmail.com>
2011-10-13 20:03:42 -0400

    simple test of basePlayer class functionality

--- main.cpp
+++ main.cpp
@@ -2,18 +2,35 @@
  * File:   main.cpp
  * Author: javier.rivera
  *
- * Created on October 13, 2011, 12:36 AM
+ * Created on September 11, 2011, 12:48 AM
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include "basePlayer.h"
+
 
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+//    vector<int> justavector(5);
+//    justavector.push_back(42);
+//    cout << "Vector size is " << justavector.size() << endl;
 
+    basePlayer bp;
+    
+    bp.setName("Jackson");
+    cout << "Player name Changed to: " << bp.getName() << endl;
+    
+    bp.setNumWins(7);
+    cout << "Player Wins changed to: " << bp.getNumWins() << endl;
+            
     return 0;
 }
 
